30d537c34e94f8cbe796d26d89ae003f813177ee
Andrey <andrii.tykhanovych.pl@gmail.com>
2020-09-23 19:30:42 +0200

    2

--- src/main/java/com/example/carsharing/controller/MainController.java
+++ src/main/java/com/example/carsharing/controller/MainController.java
@@ -3,10 +3,12 @@ package com.example.carsharing.controller;
 
 import com.example.carsharing.entity.Task;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class MainController {
 
 
